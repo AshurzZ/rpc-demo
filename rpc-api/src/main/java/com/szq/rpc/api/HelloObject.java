c231a7db07ecbc5afdf817d4694ed24201a80b92
Ashur <1021148333@qq.com>
2022-03-02 00:40:47 +0800

    基本框架

---
+++ rpc-api/src/main/java/com/szq/rpc/api/HelloObject.java
@@ -0,0 +1,33 @@
+package com.szq.rpc.api;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @author Ashur
+ * @desc 动态规划一般用于求最大值问题
+ * 链表题可看情况添加伪节点，可以不用单独判断头节点为空的情况
+ * 二维数组题可看情况增加一行一列，不用单独处理特殊情况
+ * 所有二叉树的题都要先考虑遍历方式
+ * 优先队列(堆)的思路是很朴素的。由于找第 K 大元素，其实就是整个数组排序以后后半部分最小的那个元素。因此，我们可以维护一个有 K 个元素的最小堆：
+ * new PriorityQueue<>((x,y)->(y-x));//修改默认参数成大根堆
+ * 面试的时候经常碰见诸如获取倒数第k个元素，获取中间位置的元素，
+ * 判断链表是否存在环，判断环的长度等和长度与位置有关的问题。这些问题都可以通过灵活运用双指针来解决。
+ * 对链表操作记得把最后一位的下一位置为null
+ * Arrays.sort(intervals, (v1, v2) -> v1[0] - v2[0]); 假设传来两个值，v1 与 v2，那么他们的先后顺序以 v1[0] 比 v2[0] 的结果为准，
+ * 即：若 v1[0] < v2[0] 则 v1 < v2，若 = 则 =，若 > 则 >
+ */
+@Data
+//自动加上所有属性的get set toString hashCode equals方法
+@AllArgsConstructor
+//添加一个含有所有已声明字段属性参数的构造函数
+public class HelloObject implements Serializable {
+    //Serializable序列化接口没有任何方法或者字段，只是用于标识可序列化的语义。
+    //实现了Serializable接口的类可以被ObjectOutputStream转换为字节流。
+    //同时也可以通过ObjectInputStream再将其解析为对象。
+    //序列化是指把对象转换为字节序列的过程;反序列化则是把持久化的字节文件数据恢复为对象的过程。
+    private Integer id;
+    private String message;
+}
