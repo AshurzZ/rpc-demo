6f8235062d65980f8b382b97e51bc93487f12a2d
Ashur <1021148333@qq.com>
2022-03-13 04:44:15 +0800

    增加Netty客户端连接失败重试机制+改变Socket方式数据传输的端序，使其与Netty保持一致

--- rpc-core/src/main/java/com/szq/rpc/util/ObjectReader.java
+++ rpc-core/src/main/java/com/szq/rpc/util/ObjectReader.java
@@ -54,15 +54,15 @@ public class ObjectReader {
     }
     /**
      * @description 字节数组转换为Int
-     * @param [src]
+     * @param src
      * @return [int]
      */
     private static int bytesToInt(byte[] src) {
         int value;
-        value = (src[0] & 0xFF)
-                | ((src[1] & 0xFF)<<8)
-                | ((src[2] & 0xFF)<<16)
-                | ((src[3] & 0xFF)<<24);
+        value = ((src[0] & 0xFF) << 24)
+                |((src[1] & 0xFF) << 16)
+                |((src[2] & 0xFF) << 8)
+                |(src[3] & 0xFF);
         return value;
     }
 }
