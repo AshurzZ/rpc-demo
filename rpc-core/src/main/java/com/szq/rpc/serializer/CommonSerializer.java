371f7f9fc99c8fc08efff2d6f349b4207cddd1b8
Ashur <1021148333@qq.com>
2022-03-16 01:21:46 +0800

    引入心跳机制，但是暂时未生效，因为设置的客户端完成一个调用后立刻关闭连接，下个版本进行更新

--- rpc-core/src/main/java/com/szq/rpc/serializer/CommonSerializer.java
+++ rpc-core/src/main/java/com/szq/rpc/serializer/CommonSerializer.java
@@ -10,6 +10,7 @@ public interface CommonSerializer {
     Integer JSON_SERIALIZER = 1;
     Integer HESSIAN_SERIALIZER = 2;
     Integer PROTOBUF_SERIALIZER = 3;
+    Integer DEFAULT_SERIALIZER = KRYO_SERIALIZER;
     static CommonSerializer getByCode(int code) {
         switch (code) {
             case 0:
